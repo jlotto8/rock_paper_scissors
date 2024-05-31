a9f4e811070e6e9257c458df69b24a62cd5e5cbe
jlotto8 <jlotto8@gmail.com>
2024-05-31 12:46:41 -0400

    add more mini projects

---
+++ day_5.py
@@ -0,0 +1,58 @@
+'''
+Write a program that calculates avrg ht from a List of heights.
+Important You should not use the sum() or len() functions in your answer. You should try to replicate their functionality using what you have learnt about for loops.
+Example Input 1
+156 178 165 171 187
+In this case, student_heights would be a list that looks like: [156, 178, 165, 171, 187]
+Example Output 1
+total height = 857
+number of students = 5
+average height = 171
+Example Input 2
+151 145 179
+Example Output 2
+total height = 475
+number of students = 3
+average height = 158
+'''
+# student_hts = [156, 178, 165, 171, 187]
+# sum_ht = 0
+# count = 0
+# for height in student_hts:
+#     sum_ht += height
+#     count += 1
+# print(count)
+# print(sum_ht)
+# average = sum_ht/count
+# print(round(average))
+
+'''
+Write a program that calculates the highest score from a List of scores.
+Important you are not allowed to use the max or min functions.
+The highest score in the class is: x
+Example Input
+78 65 89 86 55 91 64 89
+In this case, student_scores would be a list that looks like: [78, 65, 89, 86, 55, 91, 64, 89]
+Example Output
+The highest score in the class is: 
+'''
+# Input a list of student scores
+# student_scores = input().split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+
+# highest = 0
+# for score in student_scores:
+#   if highest < score:
+#     highest = score
+# print(f'The highest score in the class is: {highest}')
+
+# target = int(input()) # Enter a number between 0 and 1000
+# # 🚨 Do not change the code above ☝️
+
+# # Write your code here 👇
+# total = 0
+# for i in range(1,target+1):
+#   if i%2 == 0:
+#     total += i
+# print(total)
---
+++ dictionaries.py
@@ -0,0 +1,60 @@
+# country = 'brazil' # Add country name
+# visits = 6 # Number of visits
+# list_of_cities = ['Sao Paolo', 'Rio'] #eval(input()) # create list from formatted string
+
+# travel_log = [
+#   {
+#     "country": "France",
+#     "visits": 12,
+#     "cities": ["Paris", "Lille", "Dijon"]
+#   },
+#   {
+#     "country": "Germany",
+#     "visits": 5,
+#     "cities": ["Berlin", "Hamburg", "Stuttgart"]
+#   },
+# ]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+# def add_new_country(country, visits, list_of_cities):
+#   travel_log[0]["country"] = country
+#   travel_log[["visits"]] = visits
+#   travel_log[["cities"]] = list_of_cities
+
+# print(travel_log)
+# Do not change the code below 👇
+# print(add_new_country(country, visits, list_of_cities))
+# print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+# print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+# print(travel_log)
+
+
+# data = [{'Address': '16 Chester st', 
+# 'City': 'Pittsfield',
+# 'Family': ['Joe', 'Jess', 'Evie', 'Vera']
+# }]
+
+
+# d = {'Name': 'Jess', 'Age': 38, 'Phone': '413.841.0521'}
+
+# for key in d.items():
+#     print(key)
+    # print(value)
+
+# print(data[0]["Family"])
+# print(d['Age'])
+
+# count = {}
+# word = 'aaaAabbBB'
+# # num = 0
+# for char in word:
+#     count[char] = count.get(char,0)+1
+# print(count)
+
+# for i in range(10):
+#     print(i)
+
+x = 473 *2
+print(x)
